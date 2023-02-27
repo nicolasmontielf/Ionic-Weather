f95b5d42e1ed975d51567fa249d1cee01c6a3ba5
Nicolás Montiel <nicolasmontielf@gmail.com>
2023-02-26 23:29:32 -0300

    Storing timezone for future feature

--- src/services/WeatherForecast.ts
+++ src/services/WeatherForecast.ts
@@ -1,5 +1,6 @@
 import { Weather } from '@/types/weather'
 import axios from 'axios'
+import store from '@/services/Storage'
 
 const WMO = {
     0: 'Clear sky',
@@ -36,7 +37,24 @@ export default class WeatherForecast {
     weather: Weather = {} as Weather;
 
     async loadWeatherInfo(): Promise<void> {
-        const { data } = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=-25.31&longitude=-57.53&timezone=America/Sao_Paulo&current_weather=true&start_date=2023-02-26&end_date=2023-03-02&daily=temperature_2m_max,temperature_2m_min&daily=sunrise,sunset&daily=weathercode&hourly=apparent_temperature,relativehumidity_2m,cloudcover,visibility,shortwave_radiation');
+        const coords = await store.get('geolocation')
+
+        const { data } = await axios.get('https://api.open-meteo.com/v1/forecast', {
+            params: {
+                latitude: coords.latitude,
+                longitude: coords.longitude,
+                timezone: 'America/Sao_Paulo',
+                current_weather: true,
+                start_date: '2023-02-26',
+                end_date: '2023-03-02',
+                daily: ['temperature_2m_max', 'temperature_2m_min', 'sunrise', 'sunset', 'weathercode'],
+                hourly: ['apparent_temperature', 'relativehumidity_2m', 'cloudcover', 'visibility', 'shortwave_radiation']
+            }
+        })
+
+        if (!(await store.get('timezone'))) {
+            await store.set('timezone', data.timezone)
+        }
 
         this.weather = data;
     }
