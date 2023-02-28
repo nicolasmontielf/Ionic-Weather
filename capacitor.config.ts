1232eccb501c4fb27c74ccc935848d99aa814eea
Nicolás Montiel <nicolasmontielf@gmail.com>
2023-02-28 03:17:56 -0300

    App name and complitation stuffs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'io.ionic.starter',
-    appName: 'weather-nico',
+    appName: 'Weather Nico',
     webDir: 'dist',
     bundledWebRuntime: false
 };
